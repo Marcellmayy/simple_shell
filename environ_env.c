6e949fe3955558eb7cf140ef2d612428500608ef
Marcellmayy <okoubimarcellmay@yahoo.com>
2023-10-18 22:59:12 -0700

    tried to fix

--- environ_env.c
+++ environ_env.c
@@ -4,6 +4,7 @@
  * jm_env - returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
+ * Return: Array of strings (char**) containing the environment variables
  * Return: Always 0
  */
 char **jm_env(info_t *info)
